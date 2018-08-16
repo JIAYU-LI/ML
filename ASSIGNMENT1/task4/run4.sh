da7a4197934710a3e3f5c3fcec9e69adce5a363c
JIAYU-LI <41449144+JIAYU-LI@users.noreply.github.com>
2018-08-16 15:57:59 +0100

    Delete run4.sh

--- ASSIGNMENT1/task4/run4.sh
+++
@@ -1,2 +0,0 @@
-hadoop jar /opt/hadoop/hadoop-2.7.3/share/hadoop/tools/lib/hadoop-streaming-2.7.3.jar -D mapred.reduce.tasks=100 -input /user/$USER/data/task2-output -output /user/$USER/data/task4-output -mapper mapper4.py -file mapper4.py -reducer reducer4.py -file reducer4.py
-
