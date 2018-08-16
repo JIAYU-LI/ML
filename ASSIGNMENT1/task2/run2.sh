1deb7906f00602f7f2d8e9307d526ec14e01cbd7
JIAYU-LI <41449144+JIAYU-LI@users.noreply.github.com>
2018-08-16 16:00:15 +0100

    Delete run2.sh

--- ASSIGNMENT1/task2/run2.sh
+++
@@ -1,1 +0,0 @@
-hadoop jar /opt/hadoop/hadoop-2.7.3/share/hadoop/tools/lib/hadoop-streaming-2.7.3.jar -input /user/$USER/data/task1-output -output /user/$USER/data/task2-output -mapper mapper2.py -file mapper2.py -reducer reducer2.py -file reducer2.py
